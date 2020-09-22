9761d87f1865a2bc3984ba49d0d7158e936fe522
Manuel Fernández <mgnr7@hotmail.com>
2020-09-22 15:48:16 -0600

    Correccion clase Tipo

---
+++ Laboratorio_3/Laboratorio_3/Tipo.cpp
@@ -0,0 +1,19 @@
+#include "TipoProyecto.h"
+
+Tipo::Tipo()
+{
+}
+
+Tipo::~Tipo()
+{
+}
+
+Tipo Tipo::obt_Tipo()
+{
+	return Tipo();
+}
+
+Tipo Tipo::tipo_sub_Actividad()
+{
+	return Tipo();
+}
---
+++ Laboratorio_3/Laboratorio_3/Tipo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Tipo
+{
+public:
+	Tipo();
+	~Tipo();
+
+	Tipo obt_Tipo();
+	Tipo tipo_sub_Actividad();
+
+private:
+
+
+};
