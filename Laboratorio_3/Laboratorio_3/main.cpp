a7b499d1ea4dee4414c19c219a9d2194b67f8ad7
Manuel Fernández <mgnr7@hotmail.com>
2020-09-24 12:25:13 -0600

    Se inicia la creacion de la funcion para pedirle al usuario el tipo y tamano del proyecto

--- Laboratorio_3/Laboratorio_3/main.cpp
+++ Laboratorio_3/Laboratorio_3/main.cpp
@@ -7,6 +7,8 @@
 #include "SerializadorAbstracto.h"
 #include "Cmpnte_Proyecto.h"
 #include "Actividad_Hoja.h"
+#include "Actividad_Grupo.h"
+#include "Tipo.h"
 
 void Serializar(Proyecto& p, SerializadorAbstracto& s) 
 {
@@ -32,8 +34,54 @@ void Serializar(Proyecto& p, SerializadorAbstracto& s)
 	cout << s.obtSerializacion() << endl;
 }
 
+/*Debe retornar un proyecto*/
+void crear_Proyecto() 
+{
+	bool ejecutar = true;
+	int cnt_niveles = 0;
+
+	while (ejecutar) 
+	{
+		cout << "Digite la cantidad de niveles del proyecto: ";
+		cin >> cnt_niveles;
+		if (cnt_niveles <= 0) 
+		{
+			cout << "La cantidad debe ser mayor a 0" << endl;
+		}
+		else 
+		{
+			/*Crear tipo con "cnt_niveles" niveles*/
+			/*Crear actiidadGrupo para la raiz*/
+			
+			vector<int> cnt_subAct;
+			int input = 0;
+
+			for (int n = 0; n < cnt_niveles; n++) 
+			{
+				cout << "Digite la cantidad de sub actividades para el nivel " + to_string(n+1) + ": ";
+				cin >> input;
+
+				cnt_subAct.push_back(input);
+
+			}
+
+			for (int i = 0; i < cnt_subAct.size(); i++)
+			{
+				/*Crear las actividades para cada nivel*/
+			}
+
+			ejecutar = false;
+		}
+	}
+
+
+	
+
+}
+
 
 int main()
 {
+	crear_Proyecto();
 }
 
